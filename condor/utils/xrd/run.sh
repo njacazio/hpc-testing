56099144f3ee4619fcc8ab4238a3d446871b6d9e
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2019-11-12 23:29:30 +0100

    Configure run.sh to run in dedicated subdir
    - printing the number of istances requested

--- condor/utils/xrd/run.sh
+++ condor/utils/xrd/run.sh
@@ -2,8 +2,10 @@
 
 echo "N workers =" $WORKERS
 echo "N events  =" $NEVENTS
-echo "Generator =" $GENERATOR 
+echo "Generator =" $GENERATOR
 echo "Extra Opt =" $EXTRAOPT
+echo "N Inst    =" $NINSTANCES
+pwd
 
 #echo "Simulation starts" >> mon.log
 o2-sim -j $WORKERS -n $NEVENTS -g $GENERATOR $EXTRAOPT |tee sim.log
@@ -11,6 +13,8 @@ o2-sim -j $WORKERS -n $NEVENTS -g $GENERATOR $EXTRAOPT |tee sim.log
 #echo "Digitization starts" >> mon.log
 #o2-sim-digitizer-workflow --tpc-lanes $WORKERS |tee digitization.log
 
-ps aux|grep domon|awk '{print "kill -9",$2}'|bash
+date
 
+echo "-- run terminated" >> ../mon.log
+echo "-- ${PWD}" >> ../mon.log
 

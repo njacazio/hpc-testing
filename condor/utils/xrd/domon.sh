5387382ecfbfe74bda87100855789bead82d128c
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2019-11-12 23:31:31 +0100

    Use new psmon function to monitor

--- condor/utils/xrd/domon.sh
+++ condor/utils/xrd/domon.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+function psmon() {
+    echo --- >> psmon.txt
+    ps -u $USER -ww -o pid=,ppid=,etimes=,cputime=,vsz=,rsz=,drs=,trs=,cmd= >> psmon.txt
+}
+
 while :
 do
-        ./getperf.py >>mon.log
-	sleep 60
+    ./getperf.py >>mon.log
+    psmon
+    sleep 60
 done

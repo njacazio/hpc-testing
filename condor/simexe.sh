189da0cd307492eb6b83e80141b9962c765d6c86
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2019-11-13 15:32:22 +0100

    Rewrite mode of submission for multi-instance
    - changed from multi singularity to multi o2-sim
    - moved code in run.sh
    - using dedicated script to check that o2-sim are alive
    - add more comments
    - rework std output

--- condor/simexe.sh
+++ condor/simexe.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cat /proc/cpuinfo
+echo "Proxy info:"
 echo $X509_USER_PROXY
 ls $X509_USER_PROXY
 
 # Parameters to be set
 export WORKERS=1
-export NEVENTS=1
-export NEVENTS=$(( $WORKERS*5 ))
-export NEVENTS=500
+export NEVENTS=$(( $WORKERS*100 ))
 export GENERATOR="pythia8"
 export EXTRAOPT="--skipModules ZDC CPV MID"
 export NINSTANCES=1
 
+# Printing simulation setup
 echo "==========="
 echo "Sim. Setup "
 echo "==========="
@@ -33,50 +33,32 @@ for I in $FILETOGET; do
     chmod a+x $I
 done
 
-
 USER=$(whoami)
+echo "Host: $(hostname)"
+echo "User: $USER"
 id $USER
 
 echo "---------------"
-
+set -x
 ls /tmp/
-
+set +x
 echo "---------------"
-
+set -x
 pwd
-
 ls -la
+set +x
 
 echo "Running run.sh"
 ./domon.sh &
 
-PROCESSES=""
-for (( n=1; n<=$NINSTANCES; n++ )); do
-    mkdir -v ./$n
-    pushd ./$n
-    for I in $FILETOGET; do
-        cp ../$I .
-    done
-    singularity exec --bind /cvmfs,/scratch_local,/marconi_scratch /cvmfs/alice-nightlies.cern.ch/singularity/alisw/slc7-builder \
-    /cvmfs/alice-nightlies.cern.ch/bin/alienv setenv VO_ALICE@AliEn-ROOT-Legacy::0.1.1-3 -c \
-    /cvmfs/alice-nightlies.cern.ch/bin/alienv setenv O2/nightly-20190710-1 -c \
-    ./run.sh 1>simlog.out 2>simlog.err &
-    PROCESSES="$PROCESSES $!"
-    echo "Sumbitted instance $n/${NINSTANCES} with PID $!"
-    popd
-done
-
-# Waiting for all processes to finish
-echo "ALL JOBS PIDs: $PROCESSES"
-for i in $PROCESSES; do
-    echo "Waiting job $i"
-    wait $i
-    echo "job done!"
-done
-
-echo "All jobs are done!"
+singularity exec --bind /cvmfs,/scratch_local,/marconi_scratch /cvmfs/alice-nightlies.cern.ch/singularity/alisw/slc7-builder \
+/cvmfs/alice-nightlies.cern.ch/bin/alienv setenv VO_ALICE@AliEn-ROOT-Legacy::0.1.1-3 -c \
+/cvmfs/alice-nightlies.cern.ch/bin/alienv setenv O2/nightly-20190710-1 -c \
+./run.sh
 
 ps aux|grep domon|awk '{print "kill -9",$2}'|bash
+
+echo "Done!"
 ls -altr
 
 echo "Printing monitoring log"
--- condor/utils/xrd/run.sh
+++ condor/utils/xrd/run.sh
@@ -8,13 +8,29 @@ echo "N Inst    =" $NINSTANCES
 pwd
 
 #echo "Simulation starts" >> mon.log
-o2-sim -j $WORKERS -n $NEVENTS -g $GENERATOR $EXTRAOPT |tee sim.log
+
+PROCESSES=""
+MONITORPROCESSES=""
+for (( n=1; n<=$NINSTANCES; n++ )); do
+    mkdir -v ./$n
+    pushd ./$n
+    o2-sim -j $WORKERS -n $NEVENTS -g $GENERATOR $EXTRAOPT 1>simlog.out 2>simlog.err &
+    PROCESSES="$PROCESSES $!"
+    echo "Sumbitted instance $n/${NINSTANCES} with PID $!"
+    ../checkjobstatus.sh "$!" &
+    MONITORPROCESSES="$MONITORPROCESSES $!"
+    popd
+done
+
+# Waiting for all processes to finish
+echo "ALL JOBS PIDs: $PROCESSES"
+for i in $MONITORPROCESSES; do
+    wait $i
+done
 
 #echo "Digitization starts" >> mon.log
 #o2-sim-digitizer-workflow --tpc-lanes $WORKERS |tee digitization.log
 
+echo "run.sh terminated!"
 date
-
-echo "-- run terminated" >> ../mon.log
-echo "-- ${PWD}" >> ../mon.log
 
